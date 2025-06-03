fc32c07d26351e56ce1e43252495586798648133
ian37000 <160811483+ian37000@users.noreply.github.com>
2025-06-02 19:35:46 -0600

    comentario

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:project_resendis/styles/app_theme.dart';
 
+//Hola luis
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
