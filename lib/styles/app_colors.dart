75ccc6cdecdaa908f31f5f80620a53b91acf24ce
ian37000 <160811483+ian37000@users.noreply.github.com>
2025-06-02 20:22:27 -0600

    hi

--- lib/styles/app_colors.dart
+++ lib/styles/app_colors.dart
@@ -6,18 +6,19 @@ class AppColors {
   static const Color primaryVariantColor = Color(0xFF3700B3);
   static const Color secondaryColor = Color(0xFF03DAC6);
   static const Color secondaryVariantColor = Color(0xFF018786);
-  
+
   // Colores de fondo
   static const Color backgroundColor = Colors.white;
   static const Color surfaceColor = Colors.white;
-  
+
   // Colores de texto
   static const Color textPrimaryColor = Color(0xFF000000);
   static const Color textSecondaryColor = Color(0xFF757575);
-  
+
   // Colores de estado
   static const Color errorColor = Color(0xFFB00020);
   static const Color successColor = Color(0xFF4CAF50);
   static const Color warningColor = Color(0xFFFFC107);
   static const Color infoColor = Color(0xFF2196F3);
 }
+//HOLA LUIS
